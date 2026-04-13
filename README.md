c5b84e299ced167a3ab68da4701a1e6ab9be8cde
neha kumari <nehachaudhary1593@gmail.com>
2026-04-13 16:06:52 +0530

    ep4-Routing

---
+++ README.md
@@ -0,0 +1,3 @@
+play with /urls 
+order of routes matters alot
+install postman and make new workspace in which make new collection 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express =require("express")
 const app=express()
-app.use("/tes",(req,res)=>{
- res.send("heloo im server")
+
+
+app.get("/user",(req,res)=>{
+    res.send({
+        firstname:"Neha",
+        lastname:"kumari"
+
+    })
 })
+app.use("/user",(req,res)=>{
+  res.send("hahhhaaa")
+})
+app.post("/user",(req,res)=>{
+    console.log("data is send")
+    res.send("data is send")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("data is deleted succesfully")
+})
+
+
 
 app.listen(7777)
